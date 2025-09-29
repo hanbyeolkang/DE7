d01a87447e3e68374b2537b04500dacce48141c3
hanbyeolkang <strongstar1210@gmail.com>
2025-09-29 13:08:07 +0900

    로그인/로그아웃, 유저 권한 부여

--- 05_Django/mysite/polls_api/serializers.py
+++ 05_Django/mysite/polls_api/serializers.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 
 class QuestionSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     class Meta:
         model = Question
-        fields = ['id', 'question_text', 'pub_date']
+        fields = ['id', 'question_text', 'pub_date', 'owner']
 
 class UserSerializer(serializers.ModelSerializer):
     questions = serializers.PrimaryKeyRelatedField(many=True, queryset=Question.objects.all())
-
     class Meta:
         model = User
         fields = ['id', 'username', 'questions']
