68e69c54cc33cec49848b1acaf8b5830295cc2db
hanbyeolkang <strongstar1210@gmail.com>
2025-10-17 14:45:35 +0900

    순위 문제 (통과)

--- 0_코딩테스트/순위.py
+++ 0_코딩테스트/순위.py
@@ -1,50 +1,32 @@
-from collections import defaultdict
-
-
-def topologicalSort(n, results):
-    graph = defaultdict(list)
-    indegree = [0] * (n + 1)        # 진입차수
-    nodes = set(range(1, n + 1))    # 전체 노드. 1부터 n까지
-
-    for winner, loser in results:
-        graph[winner].append(loser)
-        indegree[loser] += 1        # 내 앞에 진입차수 만큼 있어야 함.
-
-    orders = []
-
-    def dfs(path, indeg):
-        if len(path) == len(nodes): # 모두 방문했으면 경로 저장
-            orders.append(path[:])  # path 깊은 복사
-            return
-        
-        for node in sorted(nodes):
-            # 내 앞에 있어야 하는게 없고, 아직 방문 전이면
-            if indeg[node] == 0 and node not in path:
-                new_indeg = indeg[:]
-                new_indeg[node] = -1        # 방문 처리
-                for nxt in graph[node]:     # 내 뒤에 오는 노드들
-                    new_indeg[nxt] -= 1     # 진입차수 -1 시켜줌
-                dfs(path + [node], new_indeg)
-
-    dfs([], indegree[:])
-    return orders
-
-
-def findFixedOrder(n, orders):
-    fixed = []
-    for i in range(n):
-        # 가능한 모든 경로에서 순서가 같으면
-        same = all(order[i] == orders[0][i] for order in orders)
-        if same:
-            fixed.append(orders[0][i])
-
-    return fixed
-
-
 def solution(n, results):
-    orders = topologicalSort(n, results)    # 가능한 모든 순서. 위상 정렬.
-    fixed = findFixedOrder(n, orders)       # 모든 순서에서 고정된 순서인 노드들.
-    return len(fixed)
+    # graph[x][y] = True : x 가 y 를 이김
+    graph = [[False] * (n+1) for _ in range(n+1)]
+    for winner, loser in results:
+        graph[winner][loser] = True
+
+    # if [a, b], [b, c] then [a, c]
+    # b 가 제일 바깥 for 문에 있어야 graph[a][c] 로 인해 생기는 영향을 다시 체크할 수 있음
+    for b in range(1, n+1):
+        for a in range(1, n+1):
+            for c in range(1, n+1):
+                if graph[a][b] and graph[b][c]:
+                    graph[a][c] = True
+
+    answer = 0
+    for i in range(1, n+1):
+        knowCnt = 0     # 확실하게 알 수 있는 결과 수
+        for j in range(1, n+1):
+            if i == j:
+                continue
+
+            # i, j 둘 중에 누가 이겼는지 알면
+            if graph[i][j] or graph[j][i]:
+                knowCnt += 1
+        
+        if knowCnt == n-1:
+            answer += 1
+
+    return answer
 
 
 n = 5
