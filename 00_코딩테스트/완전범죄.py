2260ebd74e5f6d4ab2b90d9795c81fa48166e090
hanbyeolkang <strongstar1210@gmail.com>
2026-01-07 10:42:12 +0900

    dfs 에서 합산 방식으로 변경

--- 00_코딩테스트/완전범죄.py
+++ 00_코딩테스트/완전범죄.py
@@ -1,28 +1,18 @@
 def solution(info, n, m):
-    answer = n
-    visited = set()
-
-    def dfs(idx, a, b):
-        nonlocal answer
-        visited.add((idx, a, b))
-
-        if a >= n or b >= m:
-            return
-        if a >= answer:
-            return
-        if idx == len(info):
-            answer = min(answer, a)
-            return
-        
-        newA = a+info[idx][0]
-        newB = b+info[idx][1]
-        if (idx+1, newA, b) not in visited:
-            dfs(idx+1, newA, b)
-        if (idx+1, a, newB) not in visited:
-            dfs(idx+1, a, newB)
-
-    dfs(0, 0, 0)
-    return answer if answer != n else -1
+    # b/a asc, b desc
+    info.sort(key=lambda x: (x[1]/x[0], -x[1]))
+    
+    sumA, sumB = 0, 0
+    for i in info:
+        if sumB + i[1] < m:
+            sumB += i[1]
+        else:
+            sumA += i[0]
+    
+    if sumA < n:
+        return sumA
+    else:
+        return -1
 
 
 info = [[1, 2], [2, 3], [2, 1]]
